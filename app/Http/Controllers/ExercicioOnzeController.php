adfc8d3daccf0cf52c36dc8f317d32cb4b490e12
LucasRonaldo <lucasronaldoalvesdasilva@gmail.com>
2023-08-01 09:37:17 -0300

    atualizando Readme

--- app/Http/Controllers/ExercicioOnzeController.php
+++ app/Http/Controllers/ExercicioOnzeController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class ExercicioOnzeController extends Controller
 {
-    
-}
+    public function calculadora(Request $request){
+
+        switch($request->operação){
+            case 'm' || 'M':
+            }
+        }
+    }
