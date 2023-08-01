420cb2e87371c3e2840dccbc03517a5f42bb265e
LucasRonaldo <lucasronaldoalvesdasilva@gmail.com>
2023-08-01 09:25:52 -0300

    Atualizando o Readme

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExercicioOnzeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+Route::post('exercicio/um', 
+[ExercicioUmController::class, 'multiplicar']);
+
+Route::post('exercicio/dois', 
+[ExercicioDoisController::class, 'retornarMaior']);
+
+Route::post('exercicio/tres', 
+[ExercicioTresController::class, 'media']);
+
+Route::post('exercicio/quatro', 
+[ExercicioQuatroController::class, 'verificador']);
+
+Route::get('exercicio/cinco', 
+[ExercicioCincoController::class, 'divisivelporquatro']);
+
+Route::post('exercicio/seis', 
+[ExercicioSeisController::class, 'idade']);
+
+Route::get('exercicio/sete', 
+[ExercicioSeteController::class, 'exibirNumeros']);
+
+Route::post('exercicio/oito', 
+[ExercicioOitoController::class, 'exibirTabuada']);
+
+Route::post('exercicio/nove', 
+[ExercicioNoveController::class, 'divisivelPeloUsuario']);
+
+Route::get('exercicio/dez', 
+[ExercicioDezController::class, 'impares']);
+
+
+Route::post('exercicio/onze', 
+[ExercicioOnzeController::class, '']);
+
+
+
+
+
+
+
